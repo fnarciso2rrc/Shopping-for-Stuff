6fe5f7864b761437d4335c19ab98031914c9c7fb
Frans Narciso <fransnarc@gmail.com>
2024-11-19 12:13:38 -0600

    Added routes to checkout

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,19 @@
+class CheckoutController < ApplicationController
+
+    def create
+        #
+        #
+        # 
+        product = Product.find(params[:product_id])
+    end
+
+    def success
+        #
+
+    end
+
+    def cancel
+        #
+
+    end
+end

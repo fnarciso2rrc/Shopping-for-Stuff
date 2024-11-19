2223d91f96aa6117d2333510828114f3bff098fc
Frans Narciso <fransnarc@gmail.com>
2024-11-19 13:52:25 -0600

    Got payment page to work.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -6,6 +6,30 @@ class CheckoutController < ApplicationController
         #
         # 
         product = Product.find(params[:product_id])
+
+        if product.nil?
+            redirect_to root_path
+            return
+        end
+
+        @session = Stripe::Checkout::Session.create(
+            payment_method_types: ["card"],
+            success_url: checkout_success_url,
+            cancel_url: checkout_cancel_url,
+            mode: "payment",
+            line_items: [
+                    price_data: {
+                        currency: "cad",
+                        product_data: {
+                            name: product.name,
+                            description: product.description,
+                        },
+                        unit_amount: product.price_cents,
+                    },
+                    quantity: 1
+            ]
+        )
+        redirect_to @session.url, allow_other_host: true
     end
 
     def success
@@ -17,4 +41,6 @@ class CheckoutController < ApplicationController
         #
 
     end
+
+
 end
